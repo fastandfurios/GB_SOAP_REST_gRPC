be41e6a8ca35d7b443b2ead3a8fb3738aa242fca
Дмитрий <semu-dima@yandex.ru>
2022-08-07 21:17:56 +0300

    added implementation

---
+++ LibraryService/Services/Implementation/LibraryDatabaseContext.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using LibraryService.Models;
+using LibraryService.Services.Interfaces;
+using Newtonsoft.Json;
+
+namespace LibraryService.Services.Implementation
+{
+    public class LibraryDatabaseContext : ILibraryDatabaseContextService
+
+    {
+    private IList<Book> _libraryDatabase;
+
+    public IList<Book> Books => _libraryDatabase;
+
+    public LibraryDatabaseContext()
+    {
+        Initialize();
+    }
+
+    private void Initialize()
+    {
+
+        _libraryDatabase =
+            JsonConvert.DeserializeObject<List<Book>>(Encoding.UTF8.GetString(Properties.Resources.books));
+    }
+    }
+}
