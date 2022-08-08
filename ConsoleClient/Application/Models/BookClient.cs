6d6229f8828ba955eb00d259ab66b3a1858acdc0
Дмитрий <semu-dima@yandex.ru>
2022-08-08 23:52:45 +0300

    added models

---
+++ ConsoleClient/Application/Models/BookClient.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleClient.Application.Models
+{
+    public class BookClient
+    {
+        public string Title { get; set; }
+
+        public string Category { get; set; }
+
+        public string Lang { get; set; }
+
+        public int Pages { get; set; }
+
+        public int AgeLimit { get; set; }
+
+        public AuthorClient[] Authors { get; set; }
+
+        public int PublicationDate { get; set; }
+    }
+}
