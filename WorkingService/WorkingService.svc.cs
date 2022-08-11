9f7be88343c5686702ce0feeec1e4aadacac69bf
Дмитрий <semu-dima@yandex.ru>
2022-08-11 19:42:23 +0300

    renamed, changed code

---
+++ WorkingService/WorkingService.svc.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ServiceModel;
+using WorkingService.AppService.Interfaces;
+using WorkingService.AppService.Interfaces.Settings;
+using WorkingService.AppService.Services;
+using WorkingService.AppService.Services.Settings;
+
+namespace WorkingService
+{
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.PerSession)]
+    public class WorkingService : IWorkingService
+    {
+        private readonly IScriptService _scriptService;
+        private readonly IStatisticsService _statisticsService;
+        private readonly ISettingsService _settingsService;
+
+        public WorkingService()
+        {
+            _statisticsService = new StatisticsService();
+            _settingsService = new SettingsService();
+            _scriptService = new ScriptService(_settingsService, _statisticsService, Callback);
+        }
+
+        IWorkingServiceCallback Callback =>
+            OperationContext.Current.GetCallbackChannel<IWorkingServiceCallback>();
+
+        public void RunScript() => _scriptService.Run();
+
+        public void UpdateAndCompileScript(string fileName)
+        {
+            _settingsService.FileName = fileName;
+            _scriptService.Compile();
+
+        }
+
+        public void CompileScript() => _scriptService.Compile();
+    }
+}
