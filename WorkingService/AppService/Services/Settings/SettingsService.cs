c02faae4e023e8324c7e06f1db67255227044aa3
Дмитрий <semu-dima@yandex.ru>
2022-08-11 19:40:42 +0300

    added implementation

---
+++ WorkingService/AppService/Services/Settings/SettingsService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using WorkingService.AppService.Interfaces.Settings;
+
+namespace WorkingService.AppService.Services.Settings
+{
+    public class SettingsService : ISettingsService
+    {
+        public SettingsService()
+        {
+            FileName = @"C:\scripts\WorkingScript.script";
+        }
+
+        public string FileName { get; set; }
+    }
+}
---
+++ WorkingService/AppService/Services/StatisticsService.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.Serialization;
+using WorkingService.AppService.Interfaces;
+
+namespace WorkingService.AppService.Services
+{
+    [DataContract]
+    public class StatisticsService : IStatisticsService
+    {
+        [DataMember]
+        public int SuccessTacts { get; set; }
+
+        [DataMember]
+        public int ErrorTacts { get; set; }
+
+        [DataMember]
+        public int AllTacts { get; set; }
+    }
+}
