ab49c428499353c72f08c15e0643fd9c024359af
Дмитрий <semu-dima@yandex.ru>
2022-08-14 18:26:32 +0300

    added repositories

---
+++ ClinicService/Services/ConsultationRepository.cs
@@ -0,0 +1,58 @@
+﻿using ClinicService.Data.Infrastructure.Contexts;
+using ClinicService.Data.Infrastructure.Models;
+using ClinicService.Interfaces.Repositories;
+
+namespace ClinicService.Services
+{
+    public class ConsultationRepository : IConsultationRepository
+    {
+
+        #region Serives
+
+        private readonly ClinicServiceDbContext _dbContext;
+        private readonly ILogger<ConsultationRepository> _logger;
+
+        #endregion
+
+        #region Constructors
+
+        public ConsultationRepository(ClinicServiceDbContext dbContext,
+            ILogger<ConsultationRepository> logger)
+        {
+            _logger = logger;
+            _dbContext = dbContext;
+        }
+
+        #endregion
+
+        public int Add(Consultation item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Consultation item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Consultation> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Consultation? GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Consultation item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ClinicService/Services/PetRepository.cs
@@ -0,0 +1,58 @@
+﻿using ClinicService.Data.Infrastructure.Contexts;
+using ClinicService.Data.Infrastructure.Models;
+using ClinicService.Interfaces.Repositories;
+
+namespace ClinicService.Services
+{
+    public class PetRepository : IPetRepository
+    {
+
+        #region Serives
+
+        private readonly ClinicServiceDbContext _dbContext;
+        private readonly ILogger<PetRepository> _logger;
+
+        #endregion
+
+        #region Constructors
+
+        public PetRepository(ClinicServiceDbContext dbContext,
+            ILogger<PetRepository> logger)
+        {
+            _logger = logger;
+            _dbContext = dbContext;
+        }
+
+        #endregion
+
+        public int Add(Pet item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Pet item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Pet> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Pet? GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Pet item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
