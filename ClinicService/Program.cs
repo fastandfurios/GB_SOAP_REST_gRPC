2393654c102fe0cd01de1cba29fc660e2035196f
Дмитрий <semu-dima@yandex.ru>
2022-08-14 18:28:05 +0300

    registrated services

--- ClinicService/Program.cs
+++ ClinicService/Program.cs
@@ -1,15 +1,50 @@
+#region references
+using ClinicService.Data.Infrastructure.Contexts;
+using ClinicService.Interfaces.Repositories;
+using ClinicService.Services;
+using Microsoft.AspNetCore.HttpLogging;
+using Microsoft.EntityFrameworkCore;
+using NLog.Web;
+#endregion
+
+#region Add services to the container.
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddDbContext<ClinicServiceDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration["Settings:DatabaseOptions:ConnectionString"]);
+});
+
+builder.Services.AddHttpLogging(logging =>
+{
+    logging.LoggingFields = HttpLoggingFields.All | HttpLoggingFields.RequestQuery;
+    logging.RequestBodyLogLimit = 4096;
+    logging.ResponseBodyLogLimit = 4096;
+    logging.RequestHeaders.Add("Authorization");
+    logging.RequestHeaders.Add("X-Real-IP");
+    logging.RequestHeaders.Add("X-Forwarded-For");
+});
+
+
+builder.Host.ConfigureLogging(logging =>
+{
+    logging.ClearProviders();
+    logging.AddConsole();
+
+}).UseNLog(new NLogAspNetCoreOptions() { RemoveLoggerFactoryFilter = true });
+
+builder.Services.AddScoped<IPetRepository, PetRepository>();
+builder.Services.AddScoped<IConsultationRepository, ConsultationRepository>();
+builder.Services.AddScoped<IClientRepository, ClientRepository>();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+#endregion
 
+#region Configure the HTTP request pipeline.
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -17,7 +52,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseAuthorization();
-
+app.UseRouting();
 app.MapControllers();
 
 app.Run();
+#endregion
