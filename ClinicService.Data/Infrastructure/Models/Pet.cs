c3d9509150d235b26aa4464e34a9de158634dc59
Дмитрий <semu-dima@yandex.ru>
2022-08-14 18:24:54 +0300

    added models

---
+++ ClinicService.Data/Infrastructure/Models/Pet.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ClinicService.Data.Infrastructure.Models
+{
+    [Table("Pets")]
+    public class Pet
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int PetId { get; set; }
+
+        [ForeignKey(nameof(Client))]
+        public int ClientId { get; set; }
+
+        [Column]
+        [StringLength(20)]
+        public string Name { get; set; }
+
+        [Column]
+        public DateTime Birthday { get; set; }
+
+        public virtual Client Client { get; set; }
+
+        [InverseProperty(nameof(Consultation.Pet))]
+        public virtual ICollection<Consultation> Consultations { get; set; } = new HashSet<Consultation>();
+    }
+}
