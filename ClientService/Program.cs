b07a20adc3feca4cc625bb758d6c91001740fe65
Дмитрий <semu-dima@yandex.ru>
2022-08-14 22:44:26 +0300

    writed client

--- ClientService/Program.cs
+++ ClientService/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ClientServiceProtos;
+using Grpc.Net.Client;
+
+AppContext.SetSwitch(
+    "System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+using var channel = GrpcChannel.ForAddress("http://localhost:5001");
+
+ClientService.ClientServiceClient client = new ClientService.ClientServiceClient(channel);
+
+var createClientResponse = client.CreateClient(new ClientServiceProtos.CreateClientRequest
+{
+    Document = "doc",
+    FirstName = "Иванов",
+    Surname = "Иван",
+    Patronymic = "Иванович"
+});
+
+Console.WriteLine($"Client ({createClientResponse.ClientId}) created successfully.");
+
+
+var getClientsResponse = client.GetClients(new ClientServiceProtos.GetClientsRequest());
+if (getClientsResponse.ErrCode == 0)
+{
+    Console.WriteLine("Clients:");
+    Console.WriteLine("========\n");
+    foreach (var clientDto in getClientsResponse.Clients)
+    {
+        Console.WriteLine($"({clientDto.ClientId}/{clientDto.Document}) {clientDto.Surname} {clientDto.FirstName} {clientDto.Patronymic}");
+    }
+}
+
+Console.ReadKey();
