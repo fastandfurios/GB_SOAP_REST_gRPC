3b1402045bd968bd943d80b3a1f47990eb7e061d
Дмитрий <semu-dima@yandex.ru>
2022-08-15 22:22:04 +0300

    added requests

---
+++ ClinicService/Models/Requests/Pets/CreatePetRequests.cs
@@ -0,0 +1,9 @@
+﻿namespace ClinicService.Models.Requests.Pets
+{
+    public class CreatePetRequest
+    {
+        public string Name { get; set; }
+        public DateTime BirthDay { get; set; }
+        public int ClientId { get; set; }
+    }
+}
