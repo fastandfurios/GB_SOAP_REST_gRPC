48738a33ba269a59ff0939b5985da4b3393bea0f
Дмитрий <semu-dima@yandex.ru>
2022-08-15 12:34:53 +0300

    added extension

---
+++ ClinicService/Extensions/MapperEx.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using ClinicService.Mapping;
+
+namespace ClinicService.Extensions
+{
+    public static class MapperEx
+    {
+        public static IServiceCollection AddMapper(this IServiceCollection services)
+        {
+            var mapper = new MapperConfiguration(config => config.AddProfile(new MapperProfile())).CreateMapper();
+            var serviceDescriptor = new ServiceDescriptor(typeof(IMapper), mapper);
+            services.Add(serviceDescriptor);
+            return services;
+        }
+    }
+}
