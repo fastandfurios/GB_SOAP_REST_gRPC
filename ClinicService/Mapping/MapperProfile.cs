c8d5ffb2d7c27d615629ab1eaec4b9616ba075e0
Дмитрий <semu-dima@yandex.ru>
2022-08-15 12:34:40 +0300

    added mapperprofile

---
+++ ClinicService/Mapping/MapperProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using ClientServiceProtos;
+using ClinicService.Data.Infrastructure.Models;
+
+namespace ClinicService.Mapping
+{
+    public class MapperProfile : Profile
+    {
+        public MapperProfile()
+        {
+            CreateMap<Pet, Pet>();
+            CreateMap<Client, Client>();
+            CreateMap<CreateClientRequest, Client>();
+            CreateMap<Client, ClientResponse>();
+        }
+    }
+}
