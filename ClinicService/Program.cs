6c71b5b2302f9a67715e80193fb97df1fa3de8f3
Дмитрий <semu-dima@yandex.ru>
2022-08-16 14:01:14 +0300

    added services config

--- ClinicService/Program.cs
+++ ClinicService/Program.cs
@@ -84,6 +84,8 @@ app.UseEndpoints(endpoints =>
     // Communication with gRPC endpoints must be made through a gRPC client.
     // To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909
     endpoints.MapGrpcService<ClientService>();
+    endpoints.MapGrpcService<PetService>();
+    endpoints.MapGrpcService<ConsultationService>();
 });
 
 app.Run();
