8272f11a47b90d95b91c8e6e4d342a60a268b815
Дмитрий <semu-dima@yandex.ru>
2022-08-16 10:08:47 +0300

    added mappings

--- ClinicService/Mapping/MapperProfile.cs
+++ ClinicService/Mapping/MapperProfile.cs
@@ -1,6 +1,12 @@
 ﻿using AutoMapper;
 using ClientServiceProtos;
 using ClinicService.Data.Infrastructure.Models;
+using ConsultationServiceProtos;
+using PetServiceProtos;
+using CreateConsultationRequest = ClinicService.Models.Requests.Consultations.CreateConsultationRequest;
+using CreatePetRequest = ClinicService.Models.Requests.Pets.CreatePetRequest;
+using UpdateConsultationRequest = ClinicService.Models.Requests.Consultations.UpdateConsultationRequest;
+using UpdatePetRequest = ClinicService.Models.Requests.Pets.UpdatePetRequest;
 
 namespace ClinicService.Mapping
 {
@@ -10,8 +16,19 @@ namespace ClinicService.Mapping
         {
             CreateMap<Pet, Pet>();
             CreateMap<Client, Client>();
-            CreateMap<CreateClientRequest, Client>();
+            CreateMap<Consultation, Consultation>();
+
             CreateMap<Client, ClientResponse>();
+            CreateMap<ClientResponse, Client>();
+            CreateMap<Pet, PetResponse>();
+            CreateMap<Consultation, ConsultationResponse>();
+
+            CreateMap<CreateClientRequest, Client>();
+            CreateMap<UpdateClientRequest, Client>();
+            CreateMap<CreatePetRequest, Pet>();
+            CreateMap<UpdatePetRequest, Pet>();
+            CreateMap<CreateConsultationRequest, Consultation>();
+            CreateMap<UpdateConsultationRequest, Consultation>();
         }
     }
 }
