8bf25f35b1ebb69788cd49f30b80d7204b6c2371
Дмитрий <semu-dima@yandex.ru>
2022-08-16 10:09:24 +0300

    added methods

--- ClinicService/Services/ClientService.cs
+++ ClinicService/Services/ClientService.cs
@@ -43,6 +43,53 @@ namespace ClinicService.Services
             return Task.FromResult(response);
         }
 
+        public override Task<DeleteClientResponse> DeleteClient(DeleteClientRequest request, ServerCallContext context)
+        {
+            var client = _dbContext.Clients.FirstOrDefault(client => client.ClientId == request.ClientId);
+            if (client is null)
+                return Task.FromResult(new DeleteClientResponse
+                {
+                    ErrCode = 22,
+                    ErrMessage = "the user could not be deleted because he was not found"
+                });
+
+            _dbContext.Remove(client);
+            _dbContext.SaveChanges();
+
+            return Task.FromResult(new DeleteClientResponse());
+        }
+
+        public override Task<UpdateClientResponse> UpdateClient(UpdateClientRequest request, ServerCallContext context)
+        {
+            if (request == null)
+                return Task.FromResult(new UpdateClientResponse
+                {
+                    ErrCode = 1,
+                    ErrMessage = "the request was null"
+                });
+
+            var searchingClient = _dbContext.Clients.FirstOrDefault(c => c.ClientId == request.ClientId);
+            if (searchingClient == null)
+                return Task.FromResult(new UpdateClientResponse
+                {
+                    ErrCode = 21,
+                    ErrMessage = "such a user was not found"
+                });
+
+            var client = _mapper.Map<Client>(request);
+            _dbContext.Update(client);
+            _dbContext.SaveChanges();
+
+            return Task.FromResult(new UpdateClientResponse());
+        }
+
+        public override Task<ClientResponse> GetClientById(GetClientByIdRequest request, ServerCallContext context)
+        {
+            var client = _dbContext.Clients.FirstOrDefault(c => c.ClientId == request.ClientId);
+            var response = _mapper.Map<ClientResponse>(client);
+            return Task.FromResult(response);
+        }
+
         public override Task<GetClientsResponse> GetClients(GetClientsRequest request, ServerCallContext context)
         {
             var response = new GetClientsResponse();
