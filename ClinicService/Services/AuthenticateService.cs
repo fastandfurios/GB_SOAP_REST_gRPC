f28c9a723567add1cf3baae1412b83408c639d89
Дмитрий <semu-dima@yandex.ru>
2022-08-18 14:47:45 +0300

    added impl auth

---
+++ ClinicService/Services/AuthenticateService.cs
@@ -0,0 +1,163 @@
+﻿using ClinicService.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using ClinicService.Data.Infrastructure.Contexts;
+using ClinicService.Data.Infrastructure.Models;
+using ClinicService.Enums;
+using ClinicService.Interfaces;
+using ClinicService.Models.Requests.Auth;
+using ClinicService.Models.Responses;
+
+namespace ClinicService.Services
+{
+    public class AuthenticateService : IAuthenticateService
+    {
+        #region Services
+
+        private readonly IServiceScopeFactory _serviceScopeFactory;
+
+        #endregion
+
+        private readonly Dictionary<string, SessionInfo> _sessions = new();
+
+        public const string SecretKey = "kYp3s6v9y/B?E(H+";
+
+        public AuthenticateService(IServiceScopeFactory serviceScopeFactory)
+        {
+            _serviceScopeFactory = serviceScopeFactory;
+        }
+
+        public SessionInfo GetSessionInfo(string sessionToken)
+        {
+            SessionInfo sessionInfo;
+
+            lock (_sessions)
+            {
+                _sessions.TryGetValue(sessionToken, out sessionInfo);
+            }
+
+            if (sessionInfo is null)
+            {
+                using var scope = _serviceScopeFactory.CreateScope();
+                var context = scope.ServiceProvider.GetRequiredService<ClinicServiceDbContext>();
+
+                var session = context
+                        .AccountSessions
+                        .FirstOrDefault(item => item.SessionToken == sessionToken);
+
+                if (session is null)
+                    return null;
+
+                var account = context.Accounts.FirstOrDefault(item => item.AccountId == session.AccountId);
+
+                sessionInfo = GetSessionInfo(account, session);
+
+                if (sessionInfo != null)
+                {
+                    lock (_sessions)
+                    {
+                        _sessions[sessionToken] = sessionInfo;
+                    }
+                }
+            }
+
+            return sessionInfo;
+        }
+
+        public AuthenticationResponse Login(AuthenticationRequest authenticationRequest)
+        {
+            using var scope = _serviceScopeFactory.CreateScope();
+            var context = scope.ServiceProvider.GetRequiredService<ClinicServiceDbContext>();
+
+            var account =
+             !string.IsNullOrWhiteSpace(authenticationRequest.Login) ? 
+             FindAccountByLogin(context, authenticationRequest.Login) : null;
+
+            if (account is null)
+            {
+                return new AuthenticationResponse
+                {
+                    Status = AuthenticationStatus.UserNotFound
+                };
+            }
+
+            if (!PasswordUtils.VerifyPassword(authenticationRequest.Password, account.PasswordSalt, account.PasswordHash))
+            {
+                return new AuthenticationResponse
+                {
+                    Status = AuthenticationStatus.InvalidPassword
+                };
+            }
+
+            var session = new AccountSession
+            {
+                AccountId = account.AccountId,
+                SessionToken = CreateSessionToken(account),
+                TimeCreated = DateTime.Now,
+                TimeLastRequest = DateTime.Now,
+                IsClosed = false,
+            };
+
+            context.AccountSessions.Add(session);
+            context.SaveChanges();
+
+            var sessionInfo = GetSessionInfo(account, session);
+
+            lock (_sessions)
+            {
+                _sessions[sessionInfo.SessionToken] = sessionInfo;
+            }
+
+            return new AuthenticationResponse
+            {
+                Status = AuthenticationStatus.Success,
+                SessionInfo = sessionInfo
+            };
+        }
+
+        private SessionInfo GetSessionInfo(Account account, AccountSession accountSession)
+        {
+            return new SessionInfo
+            {
+                SessionId = accountSession.SessionId,
+                SessionToken = accountSession.SessionToken,
+                Account = new AccountDto
+                {
+                    AccountId = account.AccountId,
+                    EMail = account.EMail,
+                    FirstName = account.FirstName,
+                    LastName = account.LastName,
+                    SecondName = account.SecondName,
+                    Locked = account.Locked
+                }
+            };
+        }
+
+        private string CreateSessionToken(Account account)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(SecretKey);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(
+                    new[]{
+                        new Claim(ClaimTypes.NameIdentifier, account.AccountId.ToString()),
+                        new Claim(ClaimTypes.Name, account.EMail),
+                    }),
+                Expires = DateTime.UtcNow.AddMinutes(15),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+
+        private Account FindAccountByLogin(ClinicServiceDbContext context, string login)
+        {
+            return context
+                .Accounts
+                .FirstOrDefault(account => account.EMail == login);
+        }
+    }
+}
