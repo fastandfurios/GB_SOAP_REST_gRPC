063b105853436637e0969c16cdd11c55d9a8dfb4
Дмитрий <semu-dima@yandex.ru>
2022-08-18 14:48:43 +0300

    added resp auth

---
+++ ClinicService/Models/Responses/AuthenticationResponse.cs
@@ -0,0 +1,10 @@
+﻿using ClinicService.Enums;
+
+namespace ClinicService.Models.Responses
+{
+    public class AuthenticationResponse
+    {
+        public AuthenticationStatus Status { get; set; }
+        public SessionInfo SessionInfo { get; set; }
+    }
+}
