4921b15876735b0d45fd2d4ece81746cd0a48c41
Дмитрий <semu-dima@yandex.ru>
2022-08-18 14:47:26 +0300

    added interface auth

---
+++ ClinicService/Interfaces/IAuthenticateService.cs
@@ -0,0 +1,13 @@
+﻿using ClinicService.Models;
+using ClinicService.Models.Requests.Auth;
+using ClinicService.Models.Responses;
+
+namespace ClinicService.Interfaces
+{
+    public interface IAuthenticateService
+    {
+        AuthenticationResponse Login(AuthenticationRequest authenticationRequest);
+
+        public SessionInfo GetSessionInfo(string sessionToken);
+    }
+}
