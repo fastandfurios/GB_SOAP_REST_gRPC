292634baeb32643182f340cd595f5c3a1c257ddf
Дмитрий <semu-dima@yandex.ru>
2022-08-18 22:15:01 +0300

    added new helper project

---
+++ Accounts.Helper/Program.cs
@@ -0,0 +1,6 @@
+﻿using ClinicService;
+
+var (passwordSalt, passwordHash) = PasswordUtils.CreatePasswordHash("54321");
+Console.WriteLine(passwordSalt);
+Console.WriteLine(passwordHash);
+Console.ReadKey();
