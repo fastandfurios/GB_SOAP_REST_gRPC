c9b6174872d2efcb8ec4d001926136afe6d71a0b
Дмитрий <semu-dima@yandex.ru>
2022-08-21 13:53:32 +0300

    changed code

--- ChatClient/Program.cs
+++ ChatClient/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿#region usings
+using ChatClient;
+using MesssageServiceReference;
+#endregion
+
+var messageServiceClient = new MessageServiceClient("http://localhost:5195", new HttpClient());
+var clientId = messageServiceClient.ConnectAsync("Test").Result;
+
+var messageClient = new MessageClient(clientId);
+
+while (true)
+{
+    messageClient.SendMessageAsync(1002, "Hello!");
+    Console.ReadKey();
+}
