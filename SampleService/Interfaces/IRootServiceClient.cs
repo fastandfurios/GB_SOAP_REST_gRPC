995e50c4db307372df616b4f7cd9b100a3b33673
Дмитрий <semu-dima@yandex.ru>
2022-08-25 10:19:25 +0300

    added interface

---
+++ SampleService/Interfaces/IRootServiceClient.cs
@@ -0,0 +1,8 @@
+﻿namespace SampleService.Interfaces
+{
+    public interface IRootServiceClient
+    {
+        RootServiceNamespace.RootServiceClient Client { get; }
+        public Task<ICollection<RootServiceNamespace.WeatherForecast>> Get();
+    }
+}
