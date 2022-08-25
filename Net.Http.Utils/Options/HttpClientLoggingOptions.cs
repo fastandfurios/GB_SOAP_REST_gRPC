ccfa231f327b6c8d449cdb2e42df99bee63f1f3a
Дмитрий <semu-dima@yandex.ru>
2022-08-25 10:18:55 +0300

    added options

---
+++ Net.Http.Utils/Options/HttpClientLoggingOptions.cs
@@ -0,0 +1,13 @@
+﻿namespace Net.Http.Utils.Options
+{
+    public class HttpClientLoggingOptions
+    {
+        public const bool DefaultLogMultipartFormData = false;
+
+        public const int DefaultMaxBodyLength = 16384;
+
+        public bool LogMultipartFormData { get; set; } = DefaultLogMultipartFormData;
+
+        public int MaxBodyLength { get; set; } = DefaultMaxBodyLength;
+    }
+}
